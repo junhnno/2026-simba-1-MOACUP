212ccab0a97eef9a35d53a8c93e67bf3adc94835
junhnno <withardor03@gmail.com>
2026-06-22 13:57:04 +0900

    fix/tournament-admin-import

--- tournaments/admin.py
+++ tournaments/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Tournament, TournamentMatch, TournamentShare
+from .models import Tournament, TournamentMatch
 
 admin.site.register(Tournament)
 admin.site.register(TournamentMatch)
-admin.site.register(TournamentShare)
